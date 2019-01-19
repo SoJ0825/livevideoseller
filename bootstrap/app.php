2b6a00ab13aeb55479cd039dcf149d59a9293fa7
SOJ <takayashi0825@gmail.com>
2019-01-19 13:14:33 +0800

    install laravel-apidoc-generator

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -51,5 +51,7 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
+$app->register(\Mpociot\ApiDoc\ApiDocGeneratorServiceProvider::class);
+$app->configPath('apidoc');
 
 return $app;
