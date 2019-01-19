7f5598231c9d5fb6448680a67169871390bb2ff9
SOJ <takayashi0825@gmail.com>
2019-01-19 16:44:33 +0800

    generate a Order model and migration

---
+++ database/migrations/2019_01_19_084143_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('live_video_id');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->string('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
