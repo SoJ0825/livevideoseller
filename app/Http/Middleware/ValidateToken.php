ad969a29e9c44656b8cd7ffe0a1e58abf9b707da
SOJ <takayashi0825@gmail.com>
2019-01-23 00:31:49 +0800

    add ValidateToken Middleware

---
+++ app/Http/Middleware/ValidateToken.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use App\User;
+
+class ValidateToken {
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'token' => 'required|string|min:215'
+            ]
+        );
+
+        if ($validator->fails())
+        {
+            return response(['result' => 'false', 'response' => $validator->errors()->first()]);
+        }
+
+        $fb = new \Facebook\Facebook([
+            'app_id'                => '587226395035824',
+            'app_secret'            => '2c5a8bcc0f3f448b5d21adfbf71d978d',
+            'default_graph_version' => 'v3.2',
+        ]);
+
+        try
+        {
+            $response = $fb->get('/me?fields=id,name,email,picture', $request->token);
+        } catch (Facebook\Exceptions\FacebookResponseException $e)
+        {
+            Log::error($e->getMessage());
+
+            return response(['result' => 'false', 'response' => 'Graph returned an error']);
+            exit;
+        } catch (Facebook\Exceptions\FacebookSDKException $e)
+        {
+            Log::error($e->getMessage());
+
+            return response(['result' => 'false', 'response' => 'Facebook SDK returned an error']);
+            exit;
+        }
+
+        $user = User::where('fb_id', $response->getGraphUser()->getId())->first();
+        session()->put('user_id', $user->id);
+
+        return $next($request);
+    }
+}
